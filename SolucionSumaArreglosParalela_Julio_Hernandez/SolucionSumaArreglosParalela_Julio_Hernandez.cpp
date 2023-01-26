cb9a5c5304d5eaf367df42bc0b22c1756bbfa450
Julio Hernandez <osvaldosun97@gmail.com>
2023-01-26 00:18:56 -0600

    Primer aproach del proyecto

--- SolucionSumaArreglosParalela_Julio_Hernandez/SolucionSumaArreglosParalela_Julio_Hernandez.cpp
+++ SolucionSumaArreglosParalela_Julio_Hernandez/SolucionSumaArreglosParalela_Julio_Hernandez.cpp
@@ -3,9 +3,50 @@
 
 #include <iostream>
 
+#include <omp.h>
+
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
+void imprimeArreglo(float* d);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Sumando Arreglos en Paralelo!\n";
+
+    float a[N], b[N], c[N];
+
+    int i;
+
+    for (i = 0; i < N; i++) {
+      a[i] = i * 10;
+      b[i] = (i + 3) * 3.7;
+    }
+
+    int pedazos = chunk;
+
+    #pragma omp parallel for \
+    shared(a, b, c, pedazos) private(i) \
+    schedule(static, pedazos)
+    
+    for (i = 0; i < N; i++)
+      c[i] = a[i] + b[i];
+
+    std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo a: " << std::endl;
+    imprimeArreglo(a);
+    std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo b: " << std::endl;
+    imprimeArreglo(b);
+    std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo c: " << std::endl;
+    imprimeArreglo(c);
+}
+
+void imprimeArreglo(float* d) {
+  
+  for (int i = 0; i < mostrar; i++) 
+    std::cout << d[i] << "valores del arreglo a: " << " - ";
+
+  std::cout << std::endl;
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
