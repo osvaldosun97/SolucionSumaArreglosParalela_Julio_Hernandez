4a7acfc54635f15699df583ccd5904f64979242c
Julio Hernandez <osvaldosun97@gmail.com>
2023-01-28 22:51:39 -0600

    Entrega Final A01794366.

--- SolucionSumaArreglosParalela_Julio_Hernandez/SolucionSumaArreglosParalela_Julio_Hernandez.cpp
+++ SolucionSumaArreglosParalela_Julio_Hernandez/SolucionSumaArreglosParalela_Julio_Hernandez.cpp
@@ -6,47 +6,111 @@
 #include <omp.h>
 
 #define N 1000
-#define chunk 100
-#define mostrar 10
+#define chunk 120
+#define mostrar 15
 
 void imprimeArreglo(float* d);
+void imprimeArregloMayorA(float* d);
 
 int main()
 {
-    std::cout << "Sumando Arreglos en Paralelo!\n";
+	std::cout << "Sumando Arreglos en Paralelo!" << std::endl;
+	std::cout << std::endl;
 
-    float a[N], b[N], c[N];
+	float a[N], b[N], c[N];
 
-    int i;
+	int i;
 
-    for (i = 0; i < N; i++) {
-      a[i] = i * 10;
-      b[i] = (i + 3) * 3.7;
-    }
+	for (i = 0; i < N; i++) {
+		a[i] = i * 10;
+		b[i] = (i + 3) * 3.7;
+	}
 
-    int pedazos = chunk;
+	int pedazos = chunk;
 
-    #pragma omp parallel for \
+#pragma omp parallel for \
     shared(a, b, c, pedazos) private(i) \
     schedule(static, pedazos)
-    
-    for (i = 0; i < N; i++)
-      c[i] = a[i] + b[i];
 
-    std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo a: " << std::endl;
-    imprimeArreglo(a);
-    std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo b: " << std::endl;
-    imprimeArreglo(b);
-    std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo c: " << std::endl;
-    imprimeArreglo(c);
+	for (i = 0; i < N; i++)
+		c[i] = a[i] + b[i];
+
+
+	if (N < 1000)
+	{
+		std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo a: " << std::endl;
+		imprimeArreglo(a);
+		std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo b: " << std::endl;
+		imprimeArreglo(b);
+		std::cout << "Imprimiendo los primeros " << mostrar << "valores del arreglo c: " << std::endl;
+		imprimeArreglo(c);
+	}
+	else {
+
+		int nMinusTen = (N - mostrar) - 1;
+
+		if (nMinusTen > 0) {
+			std::cout << "Sumando Arreglos Mayores a 1000 elementos!" << std::endl;
+			std::cout << std::endl;
+
+			std::cout << "Imprimiendo los primeros " << mostrar << " y ultimos " << mostrar << " valores del arreglo a: " << std::endl;
+			std::cout << std::endl;
+			imprimeArregloMayorA(a);
+			std::cout << "Imprimiendo los primeros " << mostrar << " y ultimos " << mostrar << " valores del arreglo b: " << std::endl;
+			std::cout << std::endl;
+			imprimeArregloMayorA(b);
+			std::cout << "Imprimiendo los primeros " << mostrar << " y ultimos " << mostrar << " valores del arreglo c: " << std::endl;
+			std::cout << std::endl;
+			imprimeArregloMayorA(c);
+		}
+		else {
+			std::cout << "Error verificar el numero de elementos!" << std::endl;
+		}
+	}
 }
 
 void imprimeArreglo(float* d) {
-  
-  for (int i = 0; i < mostrar; i++) 
-    std::cout << d[i] << "valores del arreglo a: " << " - ";
 
-  std::cout << std::endl;
+	for (int i = 0; i < mostrar; i++) {
+
+		if (i == mostrar - 1)
+			std::cout << d[i];
+		else
+			std::cout << d[i] << " - ";
+
+	}
+
+	std::cout << std::endl;
+}
+
+void imprimeArregloMayorA(float* d) {
+
+	for (int i = 0; i < mostrar; i++) {
+
+		if (i == mostrar - 1)
+			std::cout << d[i];
+		else
+			std::cout << d[i] << " - ";
+
+	}
+
+	std::cout << " | ";
+
+	int nMinusTen = (N - mostrar) - 1;
+
+	for (int i = 0; i < mostrar; i++) {
+
+		if (i == mostrar - 1)
+			std::cout << d[nMinusTen];
+		else
+			std::cout << d[nMinusTen] << " - ";
+
+		nMinusTen++;
+
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
